53df93d80f88f56cedc793864a4fb81c6cd670f0
Zeff <zeffree@live.com>
2021-08-06 09:28:05 +0800

    changed let to const for constants

--- Connect Four/connect4.js
+++ Connect Four/connect4.js
@@ -5,8 +5,8 @@
  * board fills (tie)
  */
 
-let WIDTH = 7;
-let HEIGHT = 6;
+const WIDTH = 7;
+const HEIGHT = 6;
 
 let currPlayer = 1; // active player: 1 or 2
 let board = []; // array of rows, each row is array of cells  (board[y][x])
